c2e6bcd5efe1d813a8dd5f57dd7629942ba2466e
2dam <2dam@HZ359590.TartangaLH.eus>
2020-09-24 09:12:05 +0200

    Añadido comentario a la clase "Application"

--- NetBeansProjects/HolaMundo/src/Application/Application.java
+++ NetBeansProjects/HolaMundo/src/Application/Application.java
@@ -1,10 +1,13 @@
 package Application;
+/**
+ * This is the application class for this hello world mvc app.
+ */
 import Utilidades.Util;
 
 public class Application {
     
     public static void main(String[] args) {
-           
+        
     }
     
 }
